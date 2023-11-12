96eb78e9a37daee966cb8f320609941721a31a7d
Qichen Liu <liuqichne@gmail.com>
2023-11-12 01:10:52 +0100

    parse cmd args. horizontal table

--- src/asciit.rs
+++ src/asciit.rs
@@ -1,6 +1,38 @@
 use colored::*;
+use std::{env, process::exit};
 
 fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let cnt = args.len();
+    if cnt > 1 {
+        eprintln!("{} {}", "Wrong number of arguments: ", cnt);
+        std::process::exit(1);
+    }
+    let mut row_number = 32;
+    let mut col_number = 4;
+    for argument in args {
+        match argument.as_str() {
+            "h" => {
+                row_number = 16;
+                col_number = 8
+            }
+            "v" => {
+                row_number = 32;
+                col_number = 4
+            }
+            "--help" => {
+                println!("Usage: ");
+                println!("ascii h: print the horizontal table (16 x 8) (default)");
+                println!("ascii v: print the vertical table (32 x 4)");
+                println!("ascii --help: print this help message");
+                exit(0);
+            }
+            &_ => {
+                eprintln!("{} {}", "Wrong argument: ", argument);
+                exit(1)
+            }
+        }
+    }
     let specials: [&str; 33] = [
         "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS", "HT", "LF", "VT", "FF", "CR",
         "SO", "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB",
@@ -8,21 +40,21 @@ fn main() {
     ];
     // Header
     print!("┌");
-    print!("{}", "───────────┬".repeat(3));
+    print!("{}", "───────────┬".repeat(col_number - 1));
     print!("───────────");
     println!("┐");
-    let header = format!("│{:<3} {:<3} {:<3}", "Dec", "Hex", "Chr").repeat(4);
+    let header = format!("│{:<3} {:<3} {:<3}", "Dec", "Hex", "Chr").repeat(col_number);
 
     print!("{}", header);
     println!("│");
     print!("├");
-    print!("{}", "───────────┼".repeat(3));
+    print!("{}", "───────────┼".repeat(col_number - 1));
     println!("───────────┤");
 
-    for line in 0..32 {
-        for col in 0..4 {
+    for line in 0..row_number {
+        for col in 0..col_number {
             // Explicitly cast to u8 to avoid type ambiguity
-            let i = (line + col * 32) as u8;
+            let i = (line + col * row_number) as u8;
 
             let ch = if i.is_ascii_graphic() || i == 32 {
                 let char_i = i as char;
@@ -45,7 +77,7 @@ fn main() {
         println!("│");
     }
     print!("└");
-    print!("{}", "───────────┴".repeat(3));
+    print!("{}", "───────────┴".repeat(col_number - 1));
     print!("───────────");
     println!("┘");
 }
