ec9f1170d614595cf92bc345084e012174858fa2
Qichen Liu <liuqichne@gmail.com>
2024-09-04 17:57:42 +0800

    Fix: now print correct special characters when arg "h" && Format

--- src/asciit.rs
+++ src/asciit.rs
@@ -33,40 +33,42 @@ fn main() {
         "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS", "HT", "LF", "VT", "FF", "CR",
         "SO", "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB",
         "ESC", "FS", "GS", "RS", "US", "DEL",
-        ];
-        // Header
-        print!("┌");
-        print!("{}", "───────────┬".repeat(col_number - 1));
-        print!("───────────");
-        println!("┐");
-        let header = format!("│{:<3} {:<3} {:<3}", "Dec", "Hex", "Chr").repeat(col_number);
-        
-        print!("{}", header);
-        println!("│");
-        print!("├");
-        print!("{}", "───────────┼".repeat(col_number - 1));
-        println!("───────────┤");
-        
-        for line in 0..row_number {
-            for col in 0..col_number {
-                // Explicitly cast to u8 to avoid type ambiguity
-                let i = (line + col * row_number) as u8;
-                
-                let ch = if i.is_ascii_graphic() || i == 32 {
-                    let char_i = i as char;
-                    if char_i.is_lowercase() {
-                        format!("{}  ", char_i.to_string().bright_blue())
-                    } else if char_i.is_uppercase() {
-                        format!("{}  ", char_i.to_string().blue())
-                    } else if char_i.is_ascii_digit() {
-                        format!("{}  ", char_i.to_string().green())
-                    } else {
-                        char_i.to_string()
-                    }
-                } else if i == 127 {
-                    specials[32].to_string()
+    ];
+    // Header
+    print!("┌");
+    print!("{}", "───────────┬".repeat(col_number - 1));
+    print!("───────────");
+    println!("┐");
+    let header = format!("│{:<3} {:<3} {:<3}", "Dec", "Hex", "Chr").repeat(col_number);
+
+    print!("{}", header);
+    println!("│");
+    print!("├");
+    print!("{}", "───────────┼".repeat(col_number - 1));
+    println!("───────────┤");
+
+    // print row by row
+    for row in 0..row_number {
+        for col in 0..col_number {
+            // Explicitly cast to u8 to avoid type ambiguity
+            // i is the index of the current char
+            let i = (row + col * row_number) as u8;
+
+            let ch = if i.is_ascii_graphic() || i == 32 {
+                let char_i = i as char;
+                if char_i.is_lowercase() {
+                    format!("{}  ", char_i.to_string().bright_blue())
+                } else if char_i.is_uppercase() {
+                    format!("{}  ", char_i.to_string().blue())
+                } else if char_i.is_ascii_digit() {
+                    format!("{}  ", char_i.to_string().green())
                 } else {
-                    specials[line].to_string()
+                    char_i.to_string()
+                }
+            } else if i == 127 {
+                specials[32].to_string()
+            } else {
+                specials[i as usize].to_string()
             };
             print!("│{:<3} {:<3X} {:<3}", i, i, ch);
         }
@@ -77,7 +79,6 @@ fn main() {
     print!("───────────");
     println!("┘");
 }
-
 
 fn print_help() {
     println!(
